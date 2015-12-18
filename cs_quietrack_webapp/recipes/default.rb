b6d4b33ad2513d8e21bd7e95a59ab7b4076d1d97
Vladimir Kolesnikov <vladimir.kolesnikov@clearscale.net>
2015-12-18 11:22:10 +0600

    update wepapp cookbook default

--- cs_quietrack_webapp/recipes/default.rb
+++ cs_quietrack_webapp/recipes/default.rb
@@ -8,4 +8,12 @@
 #
 
 
+node[:deploy].each do |application, deploy|
+  deploy[:environment_variables].each do |key, value|
+    Chef::Log.info("Setting ENV[#{key}] to #{value}")
+    ENV[key] = value
+  end
+end
+
+
 
