af2e2d59aee73c3eb73ebec07273e87d8498b0e4
qt-opsworks <vladimir.kolesnikov@clearscale.net>
2015-12-21 23:26:48 +0600

    update

--- cs_quietrack_webapp/attributes/default.rb
+++ cs_quietrack_webapp/attributes/default.rb
@@ -5,7 +5,6 @@ default[:iis][:siteName]='QuieTrack'
 default[:iis][:domainName]='rt.quietrack.com'
 default[:iis][:siteBinds]='http/*:80:www.quietrack.com,http/*:80:rt.quietrack.com,http/*:80:quietrack.com,http/*:80:beta.quietrack.com,https/*:443:rt.quietrack.com'
 default[:iis][:pfx_path]='c:\rt.quietrack.com.pfx'
-default[:iis][:pfx_pwd]=''
 default[:iis][:cert_subject]='it.quietrack.com'
 
 default[:quietrack][:s3][:bucket_name]='quietrack-deploy'
@@ -20,10 +19,6 @@ default[:quietrack][:webapp][:dbconn_stag]='c:\inetpub\wwwroot\QuieTrack\Connect
 default[:quietrack][:s3][:dbconn_stag]='/QuietrackApplicationSource/ConnectionStrings.config'
 default[:quietrack][:webapp][:appsettings_stag]='c:\inetpub\wwwroot\QuieTrack\AppSettings.config'
 default[:quietrack][:s3][:appsettings_stag]='/QuietrackApplicationSource/AppSettings.config'
-
-default[:quietrack][:webapp][:key]='c:\inetpub\wwwroot\secret_key_datbag.pem'
-default[:quietrack][:s3][:secret_databag]='/Keys/secret_key_datbag.pem'
-
 
 
 
