b8ca80d992f93201bf198056c061d3968d83ecfb
qt-opsworks <vladimir.kolesnikov@clearscale.net>
2015-12-21 20:18:35 +0600

    update

--- cs_quietrack_webapp/recipes/default.rb
+++ cs_quietrack_webapp/recipes/default.rb
@@ -8,18 +8,6 @@
 #
 
 
-aws_s3_file "#{node[:quietrack][:webapp][:key]}" do
-  bucket "#{node[:quietrack][:s3][:bucket_name]}"
-  remote_path "#{node[:quietrack][:s3][:secret_databag]}"
-end
-
-
-config.vm.provision 'chef_client' do |chef|
-chef.encrypted_data_bag_secret_key_path="#{node[:quietrack][:webapp][:key]}"
-end
- 
-passwords = Chef::EncryptedDataBagItem.load("prod", "passwords")
-pfx_pw = passwords["cert"]
-Chef::Log.info("The pfx password is: '#{pfx_pw}'")
+Chef::Log.info("USER_ID: #{node[:deploy]['web_app'][:environment_variables][:pfx_key]}")
 
 
